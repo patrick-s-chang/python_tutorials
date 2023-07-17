fa7d7068d50c09bea9bad2fd1a5caf1c6b3c6192
Pat <patrick.s.chang@gmail.com>
2023-07-17 16:21:42 -0700

    Moved files to basic_python file

---
+++ basic_python/strings_tutorial.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+message = """Donkey Butter
+is absolutely delicious"""
+
+message = message.replace("Donkey", "Penis")
+
+print(message)
+
+greeting = "Konnichiwa"
+name = "slut"
+message2 = f"{greeting}, {name}. Welcome!"
+print(message2)
